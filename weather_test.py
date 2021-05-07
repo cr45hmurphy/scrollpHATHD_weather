04cc211aead429b7fb900724220a977e01ebc0da
Joshua Farrow Admin <adminjf@ftsad.com>
2021-05-07 16:52:17 -0500

    Initial commit

---
+++ weather_test.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# weather_test.py
+from extract import json_extract
+
+try:
+    import urllib.request as urllib2
+except ImportError:
+    import urllib2  # used to make web calls to weather APIs
+
+import json
+
+url_str = "https://api.weather.gov//stations/KSJT/observations/latest"
+
+try:
+    conditions = urllib2.urlopen(url_str)
+except urllib2.HTTPError as e:
+    e.msg = "{} (Did you set the API key?)".format(e.msg)
+    raise (e)
+
+json_string = conditions.read()                 # load into a json string
+parsed_cond = json.loads(json_string.decode())  # parse the string into a json catalog
+conditions.close()
+
+print(parsed_cond['properties']['dewpoint']['value'])
